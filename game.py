30390e7fcaa6a4d0b000ad2ac21f284b60ffcad4
Pucci <75160828+Enr1c0Pucc1@users.noreply.github.com>
2021-05-08 15:29:06 +0300

    Update game.py

--- game.py
+++ game.py
@@ -2,6 +2,18 @@ from pygame import *
 w = 700
 h = 500
 
+class Alpha(sprite.Sprite):
+    def __init__(self,x,y,h,w,speed,img_name):
+        super().__init__()
+        self.image = image.load(img_name)
+        self.rect = Rect(x,y,h,w)
+        self.rect.x
+        self.rect.y
+    def reset(self):
+        win.blit(self.image,(self.rect.x,self.rect.y))
+
+ball=Alpha(100,100,5,5 ,1,"Tennis_Ball.png")
+
 win = display.set_mode((w,h))
 while True:
     win.fill((255,255,255))
@@ -9,5 +21,5 @@ while True:
     for e in event.get():
         if e.type == 12:
             exit()
-    
+    ball.reset()
     display.update()
